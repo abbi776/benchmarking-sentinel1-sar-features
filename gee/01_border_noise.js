b572ea000542ef363b8f386c41c55222697a4108
Abdullah Toqeer <toqeerabdullah776@gmail.com>
2026-01-04 02:05:15 +1100

    Update 01_border_noise.js

--- gee/01_border_noise.js
+++ gee/01_border_noise.js
@@ -8,6 +8,19 @@
  */
 
 /**
+ * Adapted from:
+ * A. Mullissa et al. (2021),
+ * "Sentinel-1 SAR Backscatter Analysis Ready Data Preparation in Google Earth Engine"
+ * Remote Sensing 13(10), 1954. doi:10.3390/rs13101954
+ *
+ * Original Refined-Lee implementation:
+ * https://github.com/adugnag/gee_s1_ard
+ *
+ * Filter structure and directional statistics follow the ARD reference,
+ * with modifications for WPE context, band handling, and gamma-0 workflow.
+ */
+
+/**
  * Masks pixels based on the 'angle' band to remove swath edge artifacts.
  * * @param {ee.Image} image - Sentinel-1 image (must contain 'angle' band).
  * @param {number} [minDeg=30.63993] - Minimum valid angle.
